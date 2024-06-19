48e011408b7616d04ad6789bc2c4c0a7772ae8fc
SayfiddinMirkamolov <sayfiddinmirkamolov7@gmail.com>
2024-06-19 08:16:47 +0500

    hammasi qo'shildi

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import HomePage from "./pages/homePage";
+function App() {
+  return (
+    <Fragment>
+        <HomePage/>
+    </Fragment>
+  );
+}
+
+export default App;
